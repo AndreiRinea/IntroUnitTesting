a680f714aaeaabd0ee3183fd98e23d0a4ad6df5a
Andrei Rinea <arinea@intralinks.com>
2018-10-27 01:30:56 +0300

    param'd test

--- introUnitTesting.Tests/CalculatorTests.cs
+++ introUnitTesting.Tests/CalculatorTests.cs
@@ -1,4 +1,3 @@
-using System;
 using introUnitTesting.Models;
 using introUnitTesting.Services;
 using Xunit;
@@ -13,6 +12,16 @@ namespace introUnitTesting.Tests
             var calc = new Calculator();
             var res = calc.Calculate(1, 1, OperationType.Add);
             Assert.Equal(2, res);
+        }
+
+        [Theory]
+        [InlineData(1, 1, OperationType.Add, 2.0d)]
+        [InlineData(1, 1, OperationType.Subtract, 0.0d)]
+        public void Test_Calculate(int o1, int o2, OperationType type, double expRes)
+        {
+            var calc = new Calculator();
+            var res = calc.Calculate(o1, o2, type);
+            Assert.Equal(expRes, res);
         }
     }
 }
